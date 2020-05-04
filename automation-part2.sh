1103488af116dbfa2608dffaeb398853cefbac38
Gabriela Trindade <g.trindadi@gmail.com>
2020-05-04 15:26:00 +0000

    added sending the email when there is no data to update in the database

--- automation-part2.sh
+++ automation-part2.sh
@@ -11,10 +11,11 @@ today_is=$(date +"%Y-%m-%d")
 if [[  10#$covid_today_date == 10#$today_is ]]
 then
 
-    psql -U gtrindadi -d covid-19 -c "COPY covid_countries FROM '/home/gtrindadi/covid-19/covid-19-today.csv' DELIMITER ',';"
+    psql -U gtrindadi -d covid-19 -c "COPY covid_countries FROM '/home/gtrindadi/covid-19/covid-19-today.csv' DELIMITER ',';
 
 else
 
     echo 'There is no data from today!' $(date '+%F %T') >> logfile-error.csv
+    echo 'No data from today: '$today_is | mutt -s"No data from today" gabizinha_hzs@hotmail.com
 
 fi
