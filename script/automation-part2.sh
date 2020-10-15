7861038737ab811305a95f252c342cf00320bd98
Gabriela Trindade <g.trindadi@gmail.com>
2020-10-15 15:16:12 +0000

    script > automation-part2.sh
    > adding:
    >> inserting information on today bra
    
    > changing:
    >> updating changed information: the "then" condition was point to a wrong file path.

--- script/automation-part2.sh
+++ script/automation-part2.sh
@@ -22,6 +22,22 @@ else
 
 fi
 
+# inserting information on today bra
+covid_bra_date=$(cut -d, -f1 ~/covid-19/data/covid-19-bra-today.csv | head -n 1)
+yesterday_was=$(date +%Y-%m-%d -d "yesterday")
+
+if [[  10#$covid_bra_date == 10#$yesterday_was ]]
+then
+
+    psql -U gtrindadi -d covid-19 -c "COPY covid_bra FROM '/home/gtrindadi/covid-19/data/covid-19-bra-today.csv' DELIMITER ',';"
+    psql -U gtrindadi -d covid-19 -c "COPY covid_bra_state FROM '/home/gtrindadi/covid-19/data/covid-19-bra-states-today.csv' DELIMITER ',';"
+
+else
+
+    echo 'There is no data from yesterday in BRA!' $(date '+%F %T') >> logfile-error.csv
+    echo 'No data from today: '$today_is | mutt -s"No data from today about BRA" gabizinha_hzs@hotmail.com
+
+fi
 
 # inserting information from previous days
 psql -U gtrindadi -d covid-19 -c "COPY covid_countries FROM '/home/gtrindadi/covid-19/data/covid-19-insert.csv' (FORMAT csv);"
@@ -31,6 +47,6 @@ psql -U gtrindadi -d covid-19 -c "COPY covid_countries FROM '/home/gtrindadi/cov
 if [[ -s ~/covid-19/data/covid-19-update.csv ]]
 then
 
-    psql -U gtrindadi -d covid-19 -f script/update_data.sql
+    psql -U gtrindadi -d covid-19 -f update_data.sql
 
 fi
